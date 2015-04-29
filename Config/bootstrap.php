8aa19e43f8a8bf16cf879a77bdf7f914abbeb279
Justin Yost <justin@loadsys.com>
2015-04-29 15:06:10 -0700

    Update the Bootstrap
    More intelligently loads files
    
    Signed-off-by: Justin Yost <justin@loadsys.com>

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 // Load BaseSerializerException class for other plugins
-App::import('Lib/Error', 'SerializersErrors.BaseSerializerException');
+App::uses('BaseSerializerException', 'SerializersErrors.Error');
 
 // Load the SerializerExceptionRenderer Class to render exceptions for a Serializable API
-App::uses('SerializerExceptionRenderer', 'SerializersErrors.Lib/Error');
+App::uses('SerializerExceptionRenderer', 'SerializersErrors.Error');
